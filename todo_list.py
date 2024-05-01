906ff946744485afc4d4f77e425d51b30c9ab0c2
Pin <pin@climax.com.tw>
2024-05-01 13:41:13 +0800

    support check item function

--- todo_list.py
+++ todo_list.py
@@ -15,35 +15,74 @@ def add_todo_list(newJob):
     todo_list.append(newJob)
 
 def remove_todo_list(jobNo):
+    if int(jobNo) > len(todo_list):
+        return todo_list
+    
     todo_list.pop(int(jobNo)-1)
     return todo_list
 
+def check_todo(jobNo):
+    if int(jobNo) > len(todo_list):
+        return todo_list
+    
+    checkBuffer = io.StringIO()
+    jobIndex = int(jobNo)-1
+    item = todo_list[jobIndex]
+    for element in range(0, len(item)):
+        checkBuffer.write("\u0336")
+        checkBuffer.write(item[element])
+
+    todo_list[jobIndex] = checkBuffer.getvalue()
+    return todo_list
+
+
 def print_todo_list():
-    buffer = io.StringIO()
-    print('list your todo here')
+    printBuffer = io.StringIO()
+    print('YOUR TODOs')
     print('-------------------')
     for index, item in enumerate(todo_list):
-        buffer.write(str(index+1))
-        buffer.write(". ")
-        buffer.write(item)
+        printBuffer.write(str(index+1))
+        printBuffer.write(". ")
+        printBuffer.write(item)
         if len(todo_list) != index+1:
-            buffer.write("\n")
+            printBuffer.write("\n")
 
-    print(buffer.getvalue())
+    if len(todo_list) == 0:
+        print("without any todo")
+    else:
+        print(printBuffer.getvalue())
+        
     print('-------------------')
 
 
+def validCommand():
+    if len(todo_list) == 0:
+        return False
+    return True
+
+inputBuffer = io.StringIO()
+inputBuffer.write("1.Add a todo\n")
+inputBuffer.write("2.Delete a todo\n")
+inputBuffer.write("3.Check a todo\n")
+inputBuffer.write("4.List all todos\n")
+
 while True:
-    todo = input("1.Add a job. \n2.Delete a job\n")
+    todo = input(inputBuffer.getvalue())
     print('\n')
 
     if todo == "1":
-        addAJob = input("add a job here\n")
+        addAJob = input("add a job\n")
         add_todo_list(addAJob)
         print_todo_list()
     elif todo == "2":
-        delAJob = input("input job number here\n")
+        delAJob = input("input job number\n")
         remove_todo_list(delAJob)
+        print_todo_list()
+    elif todo == "3":
+        checkAJob = input("input job number\n")
+        check_todo(checkAJob)
+        print_todo_list()
+    elif todo == "4":
         print_todo_list()
 
 
