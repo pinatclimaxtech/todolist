11823dac58629d01a3d38ed579da377736994fd8
Pin <pin@climax.com.tw>
2024-05-09 15:44:48 +0800

    support interface

--- todo_list.py
+++ todo_list.py
@@ -3,49 +3,64 @@ import signal
 
 todo_list = []
 
-def add_todo_list(newJob):
-    todo_list.append(newJob)
-    return todo_list
-
-def remove_todo_list(jobNo):
-    if int(jobNo) > len(todo_list):
-        return todo_list
+class InterfAction:
+    def add_todo_list(newJob): #C
+        pass;
+    def print_todo_list(): #R
+        pass;
+    def check_todo(jobNo): #U
+        pass;
+    def remove_todo_list(jobNo): #D
+        pass;
     
-    todo_list.pop(int(jobNo)-1)
-    return todo_list
 
-def check_todo(jobNo):
-    if int(jobNo) > len(todo_list):
-        return todo_list
-    
-    checkBuffer = io.StringIO()
-    jobIndex = int(jobNo)-1
-    item = todo_list[jobIndex]
-    for element in range(0, len(item)):
-        checkBuffer.write("\u0336")
-        checkBuffer.write(item[element])
+class ImplAction:   
+    def __init__(self, todo_list):
+        self.todo_list = todo_list
 
-    todo_list[jobIndex] = checkBuffer.getvalue()
-    return todo_list
+    def add_todo_list(self, newJob):
+        self.todo_list.append(newJob)
+        return self.todo_list
 
-
-def print_todo_list():
-    printBuffer = io.StringIO()
-    print('YOUR TODOs')
-    printDivider()
-    for index, item in enumerate(todo_list):
-        printBuffer.write(str(index+1))
-        printBuffer.write(". ")
-        printBuffer.write(item)
-        if len(todo_list) != index+1:
-            printBuffer.write("\n")
-
-    if len(todo_list) == 0:
-        print("without any todo")
-    else:
-        print(printBuffer.getvalue())
+    def remove_todo_list(self, jobNo):
+        if int(jobNo) > len(self.todo_list):
+            return self.todo_list
         
-    printDivider()
+        self.todo_list.pop(int(jobNo)-1)
+        return self.todo_list
+
+    def check_todo(self, jobNo):
+        if int(jobNo) > len(self.todo_list):
+            return self.todo_list
+        
+        checkBuffer = io.StringIO()
+        jobIndex = int(jobNo)-1
+        item = self.todo_list[jobIndex]
+        for element in range(0, len(item)):
+            checkBuffer.write("\u0336")
+            checkBuffer.write(item[element])
+
+        self.todo_list[jobIndex] = checkBuffer.getvalue()
+        return self.todo_list
+
+
+    def print_todo_list(self):
+        printBuffer = io.StringIO()
+        print('YOUR TODOs')
+        printDivider()
+        for index, item in enumerate(self.todo_list):
+            printBuffer.write(str(index+1))
+            printBuffer.write(". ")
+            printBuffer.write(item)
+            if len(todo_list) != index+1:
+                printBuffer.write("\n")
+
+        if len(self.todo_list) == 0:
+            print("without any todo")
+        else:
+            print(printBuffer.getvalue())
+            
+        printDivider()
 
 def printDivider():
     for _ in range(20):
@@ -69,22 +84,24 @@ inputBuffer.write("2.Delete a todo\n")
 inputBuffer.write("3.Check a todo\n")
 inputBuffer.write("other keys for print todos\n")
 
+implAction = ImplAction(todo_list)
+
 while True:
     todo = input(inputBuffer.getvalue())
     print()
 
     if todo == "1":
         addAJob = input("add a job\n")
-        add_todo_list(addAJob)
+        implAction.add_todo_list(addAJob)
     elif todo == "2":
         delAJob = input("input job number\n")
-        remove_todo_list(delAJob)
+        implAction.remove_todo_list(delAJob)
     elif todo == "3":
         checkAJob = input("input job number\n")
-        check_todo(checkAJob)
-        print_todo_list()
+        implAction.check_todo(checkAJob)
+        implAction.print_todo_list()
     else:
-        print_todo_list()
+        implAction.print_todo_list()
         continue
 
-    print_todo_list()
+    implAction.print_todo_list()
